57231b5763408e5146f74ac8a00cbec242cd9cb8
Marlon Villalona <iamvillalona@gmail.com>
2024-09-30 01:14:34 -0400

    added a listen that waits for the port

--- src/server/server.js
+++ src/server/server.js
@@ -10,10 +10,15 @@ const startServer = (options) => {
     // Para usar middlewares se usa la palabra: use (en express)
     app.use(express.static(public_path)) // Contenido estatico que ponemos disponible
 
-    app.get('*'), (req, res) => {
+    app.get('*', (req, res) => {
         const indexPath = path.join(__dirname + `../../../${public_path}/index.html`)
         res.sendFile(indexPath)
-    }
+    })
+
+    // Para abrir un puerto y escuchar lo que le mandan
+    app.listen(port, () => {
+        console.log(`Escuchando en el puerto ${port}`);
+    })
 
 }
 
